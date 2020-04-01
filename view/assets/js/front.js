d486754b4983d24d37f419f963848c1a24f4fb37
sajad1993gh <sajad1993gh@gmail.com>
2020-03-31 20:22:33 -0400

    edited front.js

--- view/assets/js/front.js
+++ view/assets/js/front.js
@@ -1,22 +1,16 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
-  
-  $(document).ready(function() {
+$(document).ready(function() {
 
-    function getData() {
-      $.ajax({
-          url: "/",
-          method: "GET"
-      }).then(function(response) {
-          console.log(response);
-      });
-    };
-    getData();
+  $('.sidenav').sidenav();
+
+  function getData() {
+    $.ajax({
+        url: "/",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+  };
+  getData();
 
 });
