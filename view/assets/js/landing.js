cf512a8be7dd40332532d22a32371494bfa728bb
sajad1993gh <sajad1993gh@gmail.com>
2020-04-25 14:27:59 -0400

    added updated landing

--- view/assets/js/landing.js
+++ view/assets/js/landing.js
@@ -1,15 +1,13 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-// (function($){
-//   $(function(){
-
-//     $('.sidenav').sidenav();
-
-//   }); // end of document ready
-// })(jQuery); // end of jQuery name space
-
 $(document).ready(function() {
 
   $('.sidenav').sidenav();
+  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
   function getData() {
     $.ajax({
         url: "/test1",
