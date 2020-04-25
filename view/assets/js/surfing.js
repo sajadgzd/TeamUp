d7fd6a0ef47fb351059df2c1c3c85bca7e1b7f62
sajad1993gh <sajad1993gh@gmail.com>
2020-04-25 15:08:31 -0400

    updated surfing

--- view/assets/js/surfing.js
+++ view/assets/js/surfing.js
@@ -2,11 +2,6 @@
 $(document).ready(function() {
 
   $('.sidenav').sidenav();
-  $('.carousel').carousel();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
 
   function getData() {
     $.ajax({
