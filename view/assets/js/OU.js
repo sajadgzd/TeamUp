117731359b3db81566067a700779218a01bfac15
sajad1993gh <sajad1993gh@gmail.com>
2020-04-25 17:18:37 -0400

    updated OU page

---
+++ view/assets/js/OU.js
@@ -0,0 +1,47 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(document).ready(function() {
+
+  $('.sidenav').sidenav();
+  $('.chips').chips();
+  $('.chips-placeholder').chips({
+    placeholder: 'Enter a username',
+    secondaryPlaceholder: '+Interest',
+  });
+
+  var chip = {
+    tag: 'chip content',
+    image: '', //optional
+  };
+  var instance = M.Chips.getInstance(elem);
+  instance.selectChip(2); // Select 2nd chip
+
+  function getData() {
+    $.ajax({
+        url: "/test1",
+        method: "GET"
+    }).then(function(response) {
+        console.log("GET root worked fine\n",response);
+        $("#test1").append("<p style='font-weight: bold'> Type: " + response.tasks[0].description + "</p><br>");
+    });
+  };
+  getData();
+
+  $(document.body).on("click", "#test2btn", function(event) {
+    event.preventDefault();
+    var text = $("#test2txt").val().trim()
+    $("#reg-form").append("<p style='font-weight: bold'> Typed: " + text + "</p><br>")
+    console.log("text value:", text)
+
+    var msg = {
+      textmsg: text 
+    }
+
+    $.post("/test2", msg)
+    .then(function(data) {
+      console.log("got data back from POST call", data.textmsg);
+      alert("POST worked...");
+    });
+
+  });
+
+});
