ea413fe9f3dc6a3f79af9fe69f3e6e3c0080cd16
Ahsan Fayyaz <ahsan@iknox.com>
2020-04-26 17:18:15 -0400

    Added Classes structure

---
+++ pythonBackend/BaseUser.py
@@ -0,0 +1,57 @@
+class BaseUser():
+
+    def __init__(self):
+        self.name = ''
+        self.email = ''
+        self.username = ''
+        self.interest = ''
+        self.cred = ''
+        self. rejection = ''
+
+
+    def login(name, email, interest, cred, ref):
+        #stub
+
+    # def inviteToGroup(username):
+
+    def handleGroupMeeting():
+        #stub
+
+    def createMeetupPoll():
+        #stub
+
+    def createWarningPoll():
+        #stub
+
+    def createPraisePoll():
+        #stub
+    
+    def createKickPoll():
+        #stub
+    
+    def createCloseGroupPoll():
+        #stub
+    
+    def issueMeetupVote():
+        #stub
+    
+    def issueWarningVote():
+        #stub
+    
+    def issuePraiseVote():
+        #stub
+    
+    def issueKickVote():
+        #stub
+    
+    def issueCloseGroupPoll():
+        #stub
+    
+    def issueCompliment():
+        #stub
+    
+    def addToWhiteBox():
+        #stub
+    
+    def addToBlackBox():
+        #stub
---
+++ pythonBackend/SuperUser.py
@@ -0,0 +1,26 @@
+from BaseUser import BaseUser
+
+class SuperUser(BaseUser):
+    def __init__(self):
+        BaseUser.__init__(self)
+
+    def handleApplication():
+        #stub
+    
+    def blacklistFromServer():
+        #stub
+
+    def reverseReputationDeduction():
+        #stub
+    
+    def shutDownGroup():
+        #stub
+    
+    def issuePointDeduction():
+        #stub
+    
+    def issuePointIncrement():
+        #stub
+
+    def banUser():
+        #stub
