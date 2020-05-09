19eb8674d6783c0ef4f679eb2cb8557b4687a1dd
sajad1993gh <sajad1993gh@gmail.com>
2020-05-08 21:06:34 -0400

    new VIP pg and VIP js

---
+++ view/assets/js/VIP.js
@@ -0,0 +1,29 @@
+// We will handle Vote and Reputation Scores here for VIP
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(document).ready(function() {
+
+  
+    // function getData() {
+    //   $.ajax({
+    //       url: "/test1",
+    //       method: "GET"
+    //   }).then(function(response) {
+    //       console.log("GET root worked fine\n",response);
+    //       // $("#test1").append("<p style='font-weight: bold'> Type: " + response.tasks[0].description + "</p><br>");
+    //   });
+    // };
+    // getData();
+  
+    $(document.body).on("click", "#vote", function(event) {
+    //   event.preventDefault();
+      // var text = $("#test2txt").val().trim()
+      // $("#reg-form").append("<p style='font-weight: bold'> Typed: " + text + "</p><br>")
+      // console.log("text value:", text)
+      // var msg = {
+      //   textmsg: text 
+      // }
+  
+    });
+  
+  });
