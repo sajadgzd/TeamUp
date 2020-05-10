9c3edcc1bc3ede007c36e23d65be4f6a4d406d03
abdulimtiaz <abdulimtiaz98@gmail.com>
2020-05-10 19:27:42 -0400

    implemented report/appeal functions and referred user point adjustment

--- pythonBackend/OrdinaryUser.py
+++ pythonBackend/OrdinaryUser.py
@@ -1,39 +1,85 @@
-from BaseUser import BaseUser
+import sqlite3
+import json
+from flask import Flask, jsonify, render_template, request, send_from_directory
 
-class OrdinaryUser(BaseUser):
-    def __init__(self):
-        BaseUser.__init__(self)
+#Helper function
+def numOfRequests(cursor):
+    cursor.execute("SELECT * FROM moderationQueue")
+    results = cursor.fetchall()
+    return len(results)
 
-    def appealReputation(appealMessage):
-        # input appealMessage
-        # Send the appealMessage to SuperUser inbox database
-        # if (append to SuperUser Inbox database status == "success"):
-        #   print("Appeal submitted successfully")
-    
-    def reportUser(UserID, reportMessage):
-        # input UserID and reportMessage
-        # Send reportMessage to the SuperUser inbox database
-        # if (append to SuperUser Inbox database status == "success"):
-        #   print("Report submitted successsfully")
+def appealReputation():
+    jsonData = request.json
+    userEmail = jsonData["email"].lower()
+    appealMessage = jsonData["appealMessage"]
 
+    connection = sqlite3.connect(r"./database.db")
+    cursor = connection.cursor()
+    number = numOfRequests(cursor)
+    number += 1
+    cursor.execute("INSERT INTO moderationRequests (subject,message,type,status,number) VALUES(?,?,?,?,?)",(userEmail,appealMessage,"REP_APPEAL","OPEN",number))
+    connection.commit()
+    connection.close()
+    return jsonify({"Success: appeal has been submitted."})
+
+def reportUser():
+    jsonData = request.json
+    targetEmail = jsonData["email"].lower()
+    reportMessage = jsonData["reportMessage"]
+
+    connection = sqlite3.connect(r"./database.db")
+    cursor = connection.cursor()
+    number = numOfRequests(cursor)
+    number += 1
+    cursor.execute("INSERT INTO moderationRequests (subject,message,type,status,number) VALUES(?,?,?,?,?)",(targetEmail,reportMessage,"REPORT","OPEN",number))
+    connection.commit()
+    connection.close()
+    return jsonify({"Success: report has been submitted."})
+
+def reportGroup():
+    jsonData = request.json
+    groupName = jsonData["groupName"].lower()
+    reportMessage = jsonData["reportMessage"]
+
+    connection = sqlite3.connect(r"./database.db")
+    cursor = connection.cursor()
+    number = numOfRequests(cursor)
+    number += 1
+    cursor.execute("INSERT INTO moderationRequests (subject,message,type,status) VALUES(?,?,?,?,?)",(groupName,reportMessage,"REPORT","OPEN",number))
+    connection.commit()
+    connection.close()
+    return jsonify({"Success: report has been submitted."})
+
+def referenceReputation():
+    jsonData = request.json
+    referredUserEmail = jsonData["referredUser"]
+    referringUserEmail = jsonData["referringUser"]
+    points = jsonData["points"] # num of points user wants to add to referred user
     
-    def reportGroup(groupName, reportMessage):
-        # input groupName and reportMessage
-        #
-        # if (groupName is in Group Database)
-        #   Send reportMessage to the SuperUser inbox database
-        #
-        #   if (append to SuperUser Inbox databae status == "success"):
-        #       print("Report submitted successsfully")
-    
-    def referenceReputation(UserID, reputationPoints):
-        # input UserID and reputationPoints that will be given
-        # 
-        # if(UserID is in User Database):
-        #   currentUser.scorepoints += reputationPoints
-        #   print("Initial reputation score has been successfully added")
-    
+    connection = sqlite3.connect(r"./database.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM  users WHERE [email] = ?", (referredUserEmail,))
+    referredUserData = list(cursor.fetchone())
+
+    cursor.execute("SELECT * FROM  users WHERE [email] = ?", (referringUserEmail,))
+    referringUserData = list(cursor.fetchone())
+
+    if points > 10 and referringUserData[5] != "VIP":
+        return jsonify({"Error": "Only VIPs can give more than 10 points to their referred user."})
+
+    elif points > 20:
+        return jsonify({"Error": "VIPs can only award 0-20 points."})
+
+    else:
+        referredUserData[4] += points
+        cursor.execute("DELETE * FROM users WHERE [email] = ?", (referredUserEmail,))
+        cursor.execute("INSERT INTO users (email,fullname,password,groupList,reputationScore,status,invitations,blacklist,whitelist,complimentsorcomplaints,inbox) VALUES (?,?,?,?,?,?,?,?,?,?,?)",tuple(referredUserData))
+        connection.commit()
+        connection.close()
+        return jsonify({"Success" : "points awarded to referred user"})
 
 
-    
-    
+
+
+
+
