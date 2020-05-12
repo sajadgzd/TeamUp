2afed16f21fda4ee54d9d1fd07aa000ff7b28eb9
sajad1993gh <sajad1993gh@gmail.com>
2020-05-12 02:42:33 -0400

    pushhh

--- view/assets/js/SU.js
+++ view/assets/js/SU.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     }
     $.post("/getUserData", JSON.stringify(emailData))
     .then(function(response) {
-      console.log("call worked with\t " + JSON.stringify(response));
+      // console.log("call worked with\t " + JSON.stringify(response));
       $("#userEmail").text(response["userData"][1]);
       $("#reputationScore").text(response["userData"][4]);
     });
@@ -47,12 +47,61 @@ $(document).ready(function() {
     }
 
     // console.log(JSON.stringify(createGroupData))
-    
+
     $.post("/createGroup", JSON.stringify(createGroupData))
     .then(function(response) {
-      console.log("call worked with\t " + JSON.stringify(response));
+      // console.log("call worked with\t " + JSON.stringify(response));
     });
 
+    location.reload();
+
   });
+
+
+  $.post("/getUserData", JSON.stringify(emailData))
+  .then(function(response) {
+    console.log("GROUUPS::::\t " + JSON.stringify(response["userData"][3]));
+
+    for(let i=0; i<response["userData"][3].length; i++){
+
+      console.log("LOOOPING")
+      let groupName = response["userData"][3][i]
+
+      groupNameJSON = {
+        groupName: groupName
+      }
+
+      $.post("/getGroupData", JSON.stringify(groupNameJSON))
+      .then(function(response) {
+        console.log("GroupData ACTIVE AND CLOSED\t " + JSON.stringify(response));
+        console.log("GroupData ACTIVE AND CLOSED\t " + JSON.stringify(response["groupData"][1]));
+
+        if((response["groupData"][1]) == "ACTIVE"){
+
+          $("#groupsDiv").append('<div class="col s12 m4">' +
+                                  `<div class="card blue-grey darken-1">` +
+                                    `<div class="card-content white-text">` +
+                                      `<span class="card-title">${response["groupData"][0]}</span>` +
+                                    `</div>` +
+                                    `<div class="card-action">` +
+                                      `<a href="/groupMainPage" target=”_blank”>View</a>` +
+                                    `</div>` +
+                                  `</div>` +
+                                `</div>`)
+        }
+
+
+
+      });
+
+
+    }
+
+
+  
+  });
+
+
+
 
 });
