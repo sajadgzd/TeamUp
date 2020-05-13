2a90fa757f6f6255f9ccce82777d90a0ebac82cf
arun-ajay <arunajay42795@gmail.com>
2020-05-12 23:04:15 -0400

    adding second super user

--- initializeDB.py
+++ initializeDB.py
@@ -53,14 +53,45 @@ def createSuperUser():
     connection.close()
 
 
-# connection = sqlite3.connect(r"./database.db")
-# cursor = connection.cursor()
-# cursor.execute("SELECT * from users WHERE [email] = ?",("arunajay@email.com",))
-# data = list(cursor.fetchone())
+def createSuperUser2():
+    #-----Database Connection------#
+    connection = sqlite3.connect(r"./database.db")
+    cursor = connection.cursor()
+    #data for the user table
+    groupList = []
+    groupList = json.dumps(groupList)
+    status = "SUPER USER"
+    invitations = []
+    invitations = json.dumps(invitations)
+    blacklist = []
+    blacklist = json.dumps(blacklist)
+    whitelist = []
+    whitelist = json.dumps(whitelist)
+    compliments = []
+    compliments = json.dumps(compliments)
+    inbox = []
+    inbox = json.dumps(inbox)
+    referredUsers = []
+    referredUsers = json.dumps(referredUsers)
+
+    rowData = []
+    rowData.append("ajay@gmail.com")
+    rowData.append("Arun Ajay")
+    rowData.append("test")
+    rowData.append(groupList)
+    rowData.append(500)
+    rowData.append(status)
+    rowData.append(invitations)
+    rowData.append(blacklist)
+    rowData.append(whitelist)
+    rowData.append(compliments)
+    rowData.append(inbox)
+    rowData.append(referredUsers)
+    cursor.execute("INSERT INTO users (email,fullname,password,groupList,reputationScore,status,invitations,blacklist,whitelist,compliments,inbox,referredUsers) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)",tuple(rowData))
+    connection.commit()
+    connection.close()
 
 
-
-
-
-clearAllTables()
-createSuperUser()
+# clearAllTables()
+# createSuperUser()
+createSuperUser2()
